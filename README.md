cc8b24d3fba9f048985fb283fcc779f10a5b4167
marco <marco.isfc@gmail.com>
2020-08-25 19:34:27 +0200

    Readme and test

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # arrayindexoverflow
+
+Reset array index in case of overflow
+
+
+Example...
+
+```js
+const arrayindexoverflow = require('../index.js');
+const array_index = 5;
+console.log(arrayindexoverflow(['a','b','c','d','e'], array_index)); //Returs "a"
+const array_index = 11;
+console.log(arrayindexoverflow(['a','b','c','d','e'], array_index)); //Returs "b"
+```
--- test/index.js
+++ test/index.js
@@ -1,7 +1,5 @@
 const arrayindexoverflow = require('../index.js');
-
-var charset = 'abcde'.split('');
-
-var index = 10;
-
-console.log(arrayindexoverflow(charset, index)); //Returs "a"
+const array_index = 5;
+console.log(arrayindexoverflow(['a','b','c','d','e'], array_index)); //Returs "a"
+const array_index = 11;
+console.log(arrayindexoverflow(['a','b','c','d','e'], array_index)); //Returs "b"
